1330cfe0d272e75c71af7a626e26a4f80685f363
keymoon <kymn0116@gmail.com>
2020-02-20 22:51:39 +0900

    add constructor

--- content_script/model/testcase.ts
+++ content_script/model/testcase.ts
@@ -1,4 +1,9 @@
 export default class TestCase {
+    constructor(input: string, output: string) {
+        this.input = input;
+        this.output = output;
+    }
+
     input: string;
     output: string;
 }
