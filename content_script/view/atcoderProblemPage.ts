967e164cf9a22739e932dcb7565912e0649682de
keymoon <kymn0116@gmail.com>
2020-02-20 22:14:26 +0900

    lint

--- content_script/view/atcoderProblemPage.ts
+++ content_script/view/atcoderProblemPage.ts
@@ -1,34 +1,28 @@
 import ContestPage from "./problemPage";
 import TestCase from "../model/testcase";
 import TestResult from "../model/testresult";
-import {Language, SourceCode} from "../model/sourcecode";
+import { Language, SourceCode } from "../model/sourcecode";
 
-let languageDict: { [index:number]: Language; } =
-{
-    3003: Language.CPlusPlus,//C++14 GCC
-    3005: Language.CPlusPlus,//C++14 Clang
-    3029: Language.CPlusPlus,//GCC
-    3030: Language.CPlusPlus,//Clang
+const languageDict: { [index: number]: Language } = {
+    3003: Language.CPlusPlus, //C++14 GCC
+    3005: Language.CPlusPlus, //C++14 Clang
+    3029: Language.CPlusPlus, //GCC
+    3030: Language.CPlusPlus, //Clang
     3006: Language.CSharp,
-    3023: Language.Python3,//Python3
-    3510: Language.Python3,//PyPy
+    3023: Language.Python3, //Python3
+    3510: Language.Python3, //PyPy
 
     //updated
-    4003: Language.CPlusPlus,//GCC
-    4004: Language.CPlusPlus,//Clang
-    4010: Language.CSharp,//.NET Core
-    4011: Language.CSharp,//Mono-mcs
-    4012: Language.CSharp,//Mono-csc
-    4006: Language.Python3,//Python3
-    4047: Language.Python3//PyPy3
+    4003: Language.CPlusPlus, //GCC
+    4004: Language.CPlusPlus, //Clang
+    4010: Language.CSharp, //.NET Core
+    4011: Language.CSharp, //Mono-mcs
+    4012: Language.CSharp, //Mono-csc
+    4006: Language.Python3, //Python3
+    4047: Language.Python3 //PyPy3
 };
 
-export default class AtCoderProblemPage extends ContestPage {
-    siteName = "atcoder";
-
-    init(): void {
-        let resultTable =
-`<table class="table table-bordered table-striped th-center hide" id="sample-test-result-table">
+const resultTable = `<table class="table table-bordered table-striped th-center hide" id="sample-test-result-table">
     <thead>
         <tr>
             <th/>
@@ -40,21 +34,26 @@ export default class AtCoderProblemPage extends ContestPage {
     <tbody>
     </tbody>
 </table>`;
-        let testButton =
-`<button type="button" id="sample-test-button" class="btn btn-info" style="margin-left: 5px">Test sample cases</button>`;
-        let submitForm = document.querySelector('#main-container form');
-        let buttonGroup = submitForm.lastElementChild.getElementsByTagName("div")[0];
+
+const testButton = `<button type="button" id="sample-test-button" class="btn btn-info" style="margin-left: 5px">
+    Test sample cases
+</button>`;
+
+export default class AtCoderProblemPage extends ContestPage {
+    siteName = "atcoder";
+
+    init(): void {
+        const submitForm = document.querySelector("#main-container form");
+        const buttonGroup = submitForm.lastElementChild.getElementsByTagName("div")[0];
         submitForm.after(resultTable);
         buttonGroup.lastElementChild.after(testButton);
 
         document.getElementById("sample-test-button").onclick = () => {
-            let langSelector =
-                <HTMLInputElement>document.querySelector("select[name='data.LanguageId']");
-            let sourceTextArea =
-                <HTMLInputElement>submitForm.querySelector('.plain-textarea');
-            let languageID = langSelector.value;
-            let source = sourceTextArea.value;
-            let language = languageDict[languageID] || Language.Other;
+            const langSelector = document.querySelector("select[name='data.LanguageId']") as HTMLInputElement;
+            const sourceTextArea = submitForm.querySelector(".plain-textarea") as HTMLInputElement;
+            const languageID = langSelector.value;
+            const source = sourceTextArea.value;
+            const language = languageDict[languageID] || Language.Other;
             this.onTestButtonClicked(new SourceCode(language, source));
         };
     }
