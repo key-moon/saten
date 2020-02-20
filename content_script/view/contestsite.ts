e8784a25208d75a0d69da844b3b476d409c80651
keymoon <kymn0116@gmail.com>
2020-02-20 18:32:44 +0900

    add setTestResult func

--- content_script/view/contestsite.ts
+++ content_script/view/contestsite.ts
@@ -1,10 +1,13 @@
 import MockSite from "./mocksite";
 import TestCase from "../model/testcase";
+import TestResult from "../model/testresult";
 
 abstract class ContestSite {
   abstract siteName: string;
 
   abstract getTestCases(): TestCase[];
+
+  abstract setTestResults(testResults: TestResult[]): void;
 
   static getCurrentContestSite(): ContestSite {
     return new MockSite();
--- content_script/view/mocksite.ts
+++ content_script/view/mocksite.ts
@@ -1,10 +1,15 @@
 import ContestSite from "./contestsite";
 import TestCase from "../model/testcase";
+import TestResult from "../model/testresult";
 
 export default class MockSite extends ContestSite {
   siteName = "mock";
 
   getTestCases(): TestCase[] {
     return [];
+  }
+
+  setTestResults(testResults: TestResult[]): void {
+
   }
 }
