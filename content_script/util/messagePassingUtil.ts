19b5540fb585bf590e56c25517b895e629bf0a27
keymoon <kymn0116@gmail.com>
2020-02-21 18:02:00 +0900

    add messageListnerUtil.ts

--- content_script/util/messagePassingUtil.ts
+++ content_script/util/messagePassingUtil.ts
@@ -1,8 +1,8 @@
 export async function sendMessage(title: string, message: any): Promise<any> {
     return new Promise((resolve, reject) => {
         chrome.runtime.sendMessage({ title: title, message: message }, response => {
-            if (response.error) reject(response.error);
-            else resolve(response);
+            if (!response.success) reject(response.error);
+            else resolve(response.res);
         });
     });
 }
