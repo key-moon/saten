2ccdefea7c783bff7b9f84772a40fe7118c7c377
keymoon <kymn0116@gmail.com>
2020-02-22 20:55:43 +0900

    add build settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 import typescript from 'rollup-plugin-typescript';
 
-export default {
+export default [{
     input: './content_script/main.ts',
     output: {
-        file: 'dist/content_script.js',
-        format: 'iife'
+        file: 'dist/content_script.js'
     },
     plugins: [
         typescript()
     ]
-}
+}, {
+    input: './background_script/main.ts',
+    output: {
+        file: 'dist/background_script.js'
+    },
+    plugins: [
+        typescript()
+    ]
+}]
