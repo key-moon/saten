1edc0be9659201044b10e4ac5c5f4bfb3df8a0bb
keymoon <kymn0116@gmail.com>
2020-02-22 15:12:06 +0900

    add dotnet assembly

---
+++ background_script/assemblies/dotnetAssembly.ts
@@ -0,0 +1,46 @@
+import './DotnetConsole'
+import '@microsoft/dotnet-js-interop';
+import RunResult from "../../shared_model/runresult";
+import Assembly from "./assembly";
+
+const binname = "DotnetExecutor";
+
+const reSplitter = DotNet.invokeMethod(binname, "GetRuntimeErrorSplitter");
+
+const timeLimit = 2000;
+
+let _lock = false;
+
+class DotnetAssembly extends Assembly {
+    _dotnetAsmRef;
+    constructor(dotNetAssemblyReference) {
+        super();
+        this._dotnetAsmRef = dotNetAssemblyReference;
+    }
+
+    async run(testCase) {
+        return new Promise<RunResult>((resolve, reject) => {
+            if (_lock) reject("locked");
+            _lock = true;
+
+            console.log("executing...");
+            DotnetConsole.discardBuffer();
+            DotnetConsole.setInput(testCase.input);
+            DotNet.invokeMethodAsync(binname, "Run", this._dotnetAsmRef, timeLimit)
+                .then((res: RunResult) => {
+                    _lock = false;
+                    res.output = DotnetConsole.getOutput();
+                    console.log(`successfully executed. \noutput :\n${res.output}`);
+                    resolve(res);
+                })
+                .catch(err => {
+                    _lock = false;
+                    console.error(`execution failed: \n${err}`);
+                    const errorSplitter = "at \\(wrapper managed-to-native\\)";
+                    const regex = new RegExp(`${reSplitter}(.*)${errorSplitter}.*${reSplitter}`, 'm');
+                    const match = err.message.match(regex);
+                    reject(match ? match[1] : err.message);
+                });
+        });
+    }
+}
---
+++ background_script/assemblies/wasmConsole.d.ts
@@ -0,0 +1,8 @@
+declare module DotnetConsole {
+    function discardBuffer(): void;
+    function printLn(x: string): void;
+    function printErr(x: string): void;
+    function readToEnd(): string;
+    function setInput(s: string);
+    function getOutput(): string;
+}
